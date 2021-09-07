8be4564c588b66af59e5dc48a5590d44ced4f24e
Dan Am <31897551+DanAmt@users.noreply.github.com>
2021-09-07 14:16:18 +0200

    Update whodunnit.sh

--- whodunnit.sh
+++ whodunnit.sh
@@ -5,9 +5,9 @@ export HISTORIES="/home/*/.bash_history /root/.bash_history"
 export SEARCHSTRING=$1
 #func:
 whodunnit() {
-             find $HISTORIES -type f -exec grep -H $SEARCHSTRING {} \; \
-             |awk -F: '{ print $1,"  -  ",$2}'
-            }
+  find $HISTORIES -type f -exec grep -H $SEARCHSTRING {} \; \
+  |awk -F: '{ print $1,"  -  ",$2}'
+}
 #main:
 if [ -n "$SEARCHSTRING" ]
 then
