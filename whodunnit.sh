bd1ff912caf928d15c7f8fa5814fc55b342c824f
Dan Am <31897551+DanAmt@users.noreply.github.com>
2021-09-07 13:53:24 +0200

    Update whodunnit.sh

--- whodunnit.sh
+++ whodunnit.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+# Stalking and control tool to search all histories for a string 
+# vars:
+export HISTORIES=/home/*/.bash_history /root/.bash_history
+#func:
+whodunnit() {
+         if [ -n $1 ]
+         then
+                  find $HISTORIES -type f -exec grep -H $1 {} \;| grep -v $0
+        else
+                  echo "Usage: $0 searchstring"
+                  echo "searches all history files for commands"
+        fi
+#main:
+whodunnit $1
